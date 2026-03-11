6c5f1fe6e7221f0ca1c09c9540193381f6aa8bb6
Alisson Simon <alissonsimon21@gmail.com>
2026-03-11 11:43:30 -0300

    feat: creating MatchRequestDTO

---
+++ src/main/java/com/springboot/match/stats/dtos/MatchRequestDTO.java
@@ -0,0 +1,10 @@
+package com.springboot.match.stats.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record MatchRequestDTO(
+        @NotBlank
+        @Size(min = 3, max = 20)
+        String mapName
+) {}
