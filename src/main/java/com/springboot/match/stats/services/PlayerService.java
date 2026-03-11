74409a5986e1802379bc62bac158bf0bf6997215
Alisson Simon <alissonsimon21@gmail.com>
2026-03-11 14:49:30 -0300

    feat: creating PlayerService

---
+++ src/main/java/com/springboot/match/stats/services/PlayerService.java
@@ -0,0 +1,70 @@
+package com.springboot.match.stats.services;
+
+import com.springboot.match.stats.dtos.PlayerRequestDTO;
+import com.springboot.match.stats.dtos.PlayerResponseDTO;
+import com.springboot.match.stats.models.Player;
+import com.springboot.match.stats.repositories.PlayerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PlayerService {
+    private static final int DEFAULT_ELO = 1000;
+
+    private final PlayerRepository repository;
+
+    public List<PlayerResponseDTO> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(player -> new PlayerResponseDTO(player.getId(), player.getNickname(), player.getElo(), player.getCreatedAt(), player.getUpdatedAt()))
+                .toList();
+    }
+
+    public PlayerResponseDTO findById(Long id) {
+        Player player = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException());
+        return toResponseDTO(player);
+    }
+
+    public PlayerResponseDTO insert(PlayerRequestDTO dto) {
+        Player player = new Player();
+
+        player.setNickname(dto.nickname());
+        player.setElo(DEFAULT_ELO);
+
+        player = repository.save(player);
+        return toResponseDTO(player);
+    }
+
+    public PlayerResponseDTO update(Long id, PlayerRequestDTO dto) {
+        Player player = repository.getReferenceById(id);
+        updateData(player, dto);
+        player = repository.save(player);
+        return toResponseDTO(player);
+    }
+
+    public void deleteAll() {
+        repository.deleteAll();
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public void updateData(Player entity, PlayerRequestDTO dto) {
+        entity.setNickname(dto.nickname());
+    }
+
+    public PlayerResponseDTO toResponseDTO(Player player) {
+        return new PlayerResponseDTO(
+                player.getId(),
+                player.getNickname(),
+                player.getElo(),
+                player.getCreatedAt(),
+                player.getUpdatedAt()
+        );
+    }
+}
