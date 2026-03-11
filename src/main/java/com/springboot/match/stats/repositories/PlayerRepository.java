5b2b9c95fb7f202afda04671cc8e2745f51840ed
Alisson Simon <alissonsimon21@gmail.com>
2026-03-11 14:49:12 -0300

    feat: creating PlayerRepository

---
+++ src/main/java/com/springboot/match/stats/repositories/PlayerRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.match.stats.repositories;
+
+import com.springboot.match.stats.models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+}
