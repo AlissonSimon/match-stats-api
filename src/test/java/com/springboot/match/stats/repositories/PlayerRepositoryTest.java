b84d9c1b021119829ec14006a9d61161ff72219c
Alisson Simon <alissonsimon21@gmail.com>
2026-03-12 17:08:36 -0300

    feat: creating PlayerRepositoryTest

---
+++ src/test/java/com/springboot/match/stats/repositories/PlayerRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.springboot.match.stats.repositories;
+
+import com.springboot.match.stats.models.Player;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@DataJpaTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class PlayerRepositoryTest {
+    @Autowired
+    TestEntityManager entityManager;
+    @Autowired
+    PlayerRepository repository;
+
+    @Test
+    void should_return_true_if_player_exists_by_nickname() {
+        Player player = new Player(null, "Zigueira", 1000, null, null);
+        entityManager.persist(player);
+
+        boolean exists = repository.existsByNickname("Zigueira");
+
+        assertTrue(exists, "True");
+    }
+
+    @Test
+    void should_return_false_if_player_does_not_exist_by_nickname() {
+        boolean exists = repository.existsByNickname("Zigueira");
+
+        assertFalse(exists, "False");
+    }
+}
