a2c4da03bbdc6ca88a78c41b6adf3565e9f1da6e
Alisson Simon <alissonsimon21@gmail.com>
2026-03-12 17:00:13 -0300

    feat: adding derived query method

--- src/main/java/com/springboot/match/stats/repositories/PlayerRepository.java
+++ src/main/java/com/springboot/match/stats/repositories/PlayerRepository.java
@@ -4,4 +4,5 @@ import com.springboot.match.stats.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    boolean existsByNickname(String nickname);
 }
