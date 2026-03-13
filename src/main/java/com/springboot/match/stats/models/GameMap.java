39771b9dc3658421963a9ecb7c90587d278330d6
Alisson Simon <alissonsimon21@gmail.com>
2026-03-13 16:46:23 -0300

    feat: creating GameMap entity

---
+++ src/main/java/com/springboot/match/stats/models/GameMap.java
@@ -0,0 +1,29 @@
+package com.springboot.match.stats.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_maps")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameMap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", unique = true)
+    private String name;
+    @Column(name = "is_active")
+    private boolean isActive;
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDate createdAt;
+}
