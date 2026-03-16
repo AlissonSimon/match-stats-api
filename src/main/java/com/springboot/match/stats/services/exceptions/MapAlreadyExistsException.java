28bcc7c6fc178f2a325fbf05fa8018743513a7f7
Alisson Simon <alissonsimon21@gmail.com>
2026-03-16 13:54:42 -0300

    feat: creating MapAlreadyExistsException

---
+++ src/main/java/com/springboot/match/stats/services/exceptions/MapAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.springboot.match.stats.services.exceptions;
+
+public class MapAlreadyExistsException extends RuntimeException {
+    public MapAlreadyExistsException() {
+        super();
+    }
+}
