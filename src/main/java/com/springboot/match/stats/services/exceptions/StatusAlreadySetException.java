b76dd404a4420b4446bc2b6bee8d079c61b767ac
Alisson Simon <alissonsimon21@gmail.com>
2026-03-16 16:56:16 -0300

    feat: creating StatusAlreadySetException

---
+++ src/main/java/com/springboot/match/stats/services/exceptions/StatusAlreadySetException.java
@@ -0,0 +1,7 @@
+package com.springboot.match.stats.services.exceptions;
+
+public class StatusAlreadySetException extends RuntimeException {
+    public StatusAlreadySetException() {
+        super();
+    }
+}
