69acd90390b6f0ccfd015668d274748eecbb288e
Alisson Simon <alissonsimon21@gmail.com>
2026-03-16 14:00:13 -0300

    feat: adding GameMap in MatchRequestDTO

--- src/main/java/com/springboot/match/stats/dtos/match/MatchRequestDTO.java
+++ src/main/java/com/springboot/match/stats/dtos/match/MatchRequestDTO.java
@@ -1,10 +1,8 @@
 package com.springboot.match.stats.dtos.match;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 
 public record MatchRequestDTO(
-        @NotBlank
-        @Size(min = 3, max = 20)
-        String mapName
+        @NotNull
+        Long gameMapId
 ) {}
