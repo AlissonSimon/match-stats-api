629258c328c31b2ac468489843d847e859f0cc25
Alisson Simon <alissonsimon21@gmail.com>
2026-03-16 10:00:59 -0300

    fix: solving JpaRepository error

--- src/main/java/com/springboot/match/stats/repositories/GameMapRepository.java
+++ src/main/java/com/springboot/match/stats/repositories/GameMapRepository.java
@@ -2,5 +2,5 @@ package com.springboot.match.stats.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameMapRepository extends JpaRepository {
+public interface GameMapRepository extends JpaRepository<GameMap, Long> {
 }
