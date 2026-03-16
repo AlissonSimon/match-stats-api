c80997219deb43fc2b647cbc815a276a8db3d36b
Alisson Simon <alissonsimon21@gmail.com>
2026-03-16 13:54:32 -0300

    feat: creating InactiveMapException

---
+++ src/main/java/com/springboot/match/stats/services/exceptions/InactiveMapException.java
@@ -0,0 +1,7 @@
+package com.springboot.match.stats.services.exceptions;
+
+public class InactiveMapException extends RuntimeException {
+    public InactiveMapException() {
+        super();
+    }
+}
