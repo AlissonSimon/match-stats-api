93dc3084518da8d14cde8b989bbbfaa53a3c74ec
Alisson Simon <alissonsimon21@gmail.com>
2026-03-16 13:56:38 -0300

    feat: creating query in GameMapRepository

--- src/main/java/com/springboot/match/stats/repositories/GameMapRepository.java
+++ src/main/java/com/springboot/match/stats/repositories/GameMapRepository.java
@@ -1,6 +1,8 @@
 package com.springboot.match.stats.repositories;
 
+import com.springboot.match.stats.models.GameMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameMapRepository extends JpaRepository<GameMap, Long> {
+    boolean existsByName(String name);
 }
